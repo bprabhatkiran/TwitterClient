67a6daa3eb0fbf5413ce69a16cfd8ccd4a064958
Prabhat Kiran <pkiran@zynga.com>
2013-10-27 19:09:35 -0700

    Moved logic to an abstract class

--- src/com/codepath/apps/restclienttemplate/TimeLineActivity.java
+++ src/com/codepath/apps/restclienttemplate/TimeLineActivity.java
@@ -1,53 +1,18 @@
 package com.codepath.apps.restclienttemplate;
 
-import java.util.ArrayList;
-
-import org.json.JSONArray;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.codepath.apps.restclienttemplate.helpers.EndlessScrollListener;
-import com.codepath.apps.restclienttemplate.models.Tweet;
-import com.loopj.android.http.JsonHttpResponseHandler;
-
-import eu.erikw.PullToRefreshListView;
-import eu.erikw.PullToRefreshListView.OnRefreshListener;
-
 public class TimeLineActivity extends FragmentActivity {
 	TweetsAdapter tweetsAdapter = null;
-	PullToRefreshListView lvTweets = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_time_line);
-		lvTweets = (PullToRefreshListView) findViewById(R.id.lvTweets);
-		lvTweets.setOnRefreshListener(new OnRefreshListener() {
-
-			@Override
-			public void onRefresh() {
-				displayTweets();
-			}
-		});
-
-		lvTweets.setOnScrollListener(new EndlessScrollListener() {
-
-			@Override
-			public void onLoadMore(int page, int totalItemsCount) {
-				if(tweetsAdapter == null) {
-					displayTweets();
-				} else {
-					displayTweets(tweetsAdapter.getCount() + 10);
-				}
-			}
-		});
-
-		displayTweets();
 	}
 
 	@Override
@@ -62,39 +27,11 @@ public class TimeLineActivity extends FragmentActivity {
 		startActivityForResult(i, 0);
 		return true;
 	}
-
-	public void displayTweets(int count) {
-		RestClientApp.getRestClient().getStatuses(count, new JsonHttpResponseHandler() {
-			@Override
-			public void onSuccess(JSONArray jsonTweets) {
-				ArrayList<Tweet> tweets = Tweet.fromJson(jsonTweets);
-				if(tweetsAdapter == null) {
-					tweetsAdapter = new TweetsAdapter(getBaseContext(), tweets);
-					lvTweets.setAdapter(tweetsAdapter);
-				} else {
-					tweetsAdapter.clear();
-					tweetsAdapter.addAll(tweets);
-				}
-				lvTweets.onRefreshComplete();
-			}
-
-			public void onFailure(Throwable e) {
-				Log.d("DEBUG", "Fetch timeline error: " + e.toString());
-			}
-		});
-	}
-
-	public void displayTweets() {
-		if(tweetsAdapter != null) {
-			tweetsAdapter.clear();
-		}
-		displayTweets(25);
-	}
-
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode == 0) {
-			// Kind of a hack, as I am not checking the result code
-			displayTweets();
-		}
-	}
+	
+//	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//		if (requestCode == 0) {
+//			// Kind of a hack, as I am not checking the result code
+//			displayTweets();
+//		}
+//	}
 }
--- src/com/codepath/apps/restclienttemplate/fragements/TweetsListFragment.java
+++ src/com/codepath/apps/restclienttemplate/fragements/TweetsListFragment.java
@@ -7,8 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.TweetsAdapter;
+import com.codepath.apps.restclienttemplate.helpers.EndlessScrollListener;
 
-public class TweetsListFragment extends Fragment {
+import eu.erikw.PullToRefreshListView;
+import eu.erikw.PullToRefreshListView.OnRefreshListener;
+
+public abstract class TweetsListFragment extends Fragment {
+	
+	TweetsAdapter tweetsAdapter;
+	PullToRefreshListView lvTweets;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -17,8 +26,41 @@ public class TweetsListFragment extends Fragment {
 	}
 	
 	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		displayTweets();
+	}
+	
+	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
+		lvTweets = (PullToRefreshListView) getActivity().findViewById(R.id.lvTweets);
+		lvTweets.setOnRefreshListener(new OnRefreshListener() {
+
+			@Override
+			public void onRefresh() {
+				displayTweets();
+			}
+		});
+
+		lvTweets.setOnScrollListener(new EndlessScrollListener() {
+
+			@Override
+			public void onLoadMore(int page, int totalItemsCount) {
+				if(tweetsAdapter == null) {
+					displayTweets();
+				} else {
+					displayTweets(tweetsAdapter.getCount() + 10);
+				}
+			}
+		});
 	}
+	
+	public void displayTweets() {
+		displayTweets(25);
+	}
+	
+	public abstract void displayTweets(int count);
 }
